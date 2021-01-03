31f74b4e09cf71972536dafce4b92442b761dccc
Stefan Ciubotaru <stefanel.ciubotaru@gmail.com>
2021-01-03 19:56:15 +0200

    Working Config without ABL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1126,8 +1126,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -18
